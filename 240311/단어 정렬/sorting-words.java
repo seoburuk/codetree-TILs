a3238ae5f317c2683635b004cdf4b056b4ad83b9
suin <101254699+seoburuk@users.noreply.github.com>
2024-03-11 13:28:10 +0900

    Title: 단어 정렬, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

--- 240311/단어 정렬/sorting-words.java
+++ 240311/단어 정렬/sorting-words.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = nextInt();
+        int n = sc.nextInt();
         String words[] = new String[n];
 
         for(int i = 0; i < n; i++) {
