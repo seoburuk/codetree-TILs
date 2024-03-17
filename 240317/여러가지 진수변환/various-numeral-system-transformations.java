069f9cc3f61d0442da3c8e499ab2c14369fb9bba
suin <101254699+seoburuk@users.noreply.github.com>
2024-03-17 21:22:36 +0900

    Title: 여러가지 진수변환, Time: 143ms, Memory: 10MB, Status: Passed - CodeTree

--- 240317/여러가지 진수변환/various-numeral-system-transformations.java
+++ 240317/여러가지 진수변환/various-numeral-system-transformations.java
@@ -2,23 +2,31 @@ import java.util.Scanner;
 
 public class Main {
     public static String change(int n, int b) {
-        StirngBuilder result = new StirngBuilder();
-        while(n > 0) {
+        StringBuilder result = new StringBuilder();
+        
+        // N이 0이면 그대로 반환
+        if (n == 0)
+            return "0";
+        
+        // N이 0보다 큰 경우에만 변환 수행
+        while (n > 0) {
             int remainder = n % b;
-            result.insert(remainder, 0);
-            n/b;
+            
+            // 나머지를 문자열에 추가
+            result.insert(0, remainder);
+            
+            // N을 B로 나눈 몫으로 갱신
+            n /= b;
         }
         
-        return result.toString;
+        return result.toString();
     }
 
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int b = sc.nextInt();
 
-        System.out(change(int n, int b));
+        System.out.println(change(n,b));
     }
 }
