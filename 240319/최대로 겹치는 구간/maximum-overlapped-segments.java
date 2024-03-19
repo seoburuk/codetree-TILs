427d6f615e84f78c4fb688ff329b89806ebc0277
suin <101254699+seoburuk@users.noreply.github.com>
2024-03-19 13:28:12 +0900

    Title: 최대로 겹치는 구간, Time: 153ms, Memory: 10MB, Status: Passed - CodeTree

--- 240319/최대로 겹치는 구간/maximum-overlapped-segments.java
+++ 240319/최대로 겹치는 구간/maximum-overlapped-segments.java
@@ -2,33 +2,33 @@ import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(); // 줄 개수
-        int[][] segement = new int[n][2]; // 선분정보 저장 배열
+        int n = sc.nextInt(); // 선분의 개수
+        int[][] segments = new int[n][2]; // 선분의 정보를 저장할 배열
 
-        for(int i = 0; i < n; i++) {
-            segement[i][0] = sc.nextInt(); // 시작점
-            segement[i][1] = sc.nextInt(); // 끝지점
-
-        } 
-
-        int[] pointCount = new int[201]; // 지점마다 겹치는지 저장하는 배열
-        // 션분을 돌면서 시작지점에 +1 마지막 지점에 -1 하면서 순회
-        for(int i = 0; i < n; i++) {
-            pointCount[segement[i][0]]++;
-            pointCount[segement[i][1]]--;
+        // 선분 정보 입력 받기
+        for (int i = 0; i < n; i++) {
+            segments[i][0] = sc.nextInt(); // 선분의 시작점
+            segments[i][1] = sc.nextInt(); // 선분의 끝점
         }
 
-        int maxSegment=0;
-        int curSegment=0;
+        // 각 점마다 몇 개의 선분이 겹치는지를 저장할 배열
+        int[] pointCount = new int[201];
 
-        for(int i = 0; i< n; i++) {
-            curSegment += pointCount[i];
-            maxSegment = Math.max(curSegment, maxSegment);
-
-
+        // 선분들을 돌면서 시작점에 +1, 끝점에 -1을 해주어서 겹치는 구간을 체크
+        for (int i = 0; i < n; i++) {
+            pointCount[segments[i][0] + 100]++; // 시작점에 +1
+            pointCount[segments[i][1] + 100]--; // 끝점에 -1
         }
-        System.out.println(maxSegment);
+
+        // 누적합을 통해 겹치는 구간을 찾고, 그 중에서 최대로 겹치는 구간의 선분 개수를 구함
+        int maxSegments = 0;
+        int currentSegments = 0;
+        for (int i = 0; i < 201; i++) {
+            currentSegments += pointCount[i];
+            maxSegments = Math.max(maxSegments, currentSegments);
+        }
+
+        System.out.println(maxSegments);
     }
 }
