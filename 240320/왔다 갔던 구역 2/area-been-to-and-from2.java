75b54f990ce71a48502dfc6e654d617bccff60f3
suin <101254699+seoburuk@users.noreply.github.com>
2024-03-20 10:57:18 +0900

    Title: 왔다 갔던 구역 2, Time: 123ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240320/왔다 갔던 구역 2/area-been-to-and-from2.java
+++ 240320/왔다 갔던 구역 2/area-been-to-and-from2.java
@@ -1,47 +1,37 @@
 import java.util.*;
-import java.io.*;
 
 public class Main {
-    public static int cnt = 0;
-    public static int here = 1000;
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num = sc.nextInt(), current = 1000, cnt = 0;
+        int[] arr = new int[2001];
 
-    public static void Findout(int x, String b, int[] arr) {
-        // Move right
-        if (b.equals("R")) {
-            for (int i = here + 1; i <= here + x; i++) {
-                if (i >= 0 && i < arr.length) {
-                    arr[i]++;
-                    if (arr[i] == 2) cnt++;
-                }
+        for (int i = 0; i < num; i++) {
+            int step = sc.nextInt();
+            char direct = sc.next().charAt(0);
+            switch (direct) {
+                case 'R':
+                    for (int j = current; j < current + step; j++) {
+                        arr[j]++;
+                    }
+                    current += step;
+                    break;
+                case 'L':
+                    for (int j = current; j > current - step; j--) {
+                        arr[j]++;
+                    }
+                    current -= step;
+                    break;
+                default:
+                    break;
             }
-            here += x;
-        } 
-        // Move left
-        else {
-            for (int i = here - 1; i >= here - x; i--) {
-                if (i >= 0 && i < arr.length) {
-                    arr[i]++;
-                    if (arr[i] == 2) cnt++;
-                }
-            }
-            here -= x;
-        }
-    }
-
-    public static int[] arr = new int[2001];
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-
-        for (int i = 0; i < n; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int x = a;
-            String b = st.nextToken();
-            Findout(x, b, arr);
         }
 
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] > 1) {
+                cnt++;
+            }
+        }
         System.out.print(cnt);
     }
 }
