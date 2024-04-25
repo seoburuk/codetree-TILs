9d182778ceff6beee7efe24c93159b0a670758eb
suin <101254699+seoburuk@users.noreply.github.com>
2024-04-26 01:00:39 +0900

    Title: 만나는 그 순간, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240426/만나는 그 순간/the-moment-we-meet.java
+++ 240426/만나는 그 순간/the-moment-we-meet.java
@@ -6,33 +6,39 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         
-        int[] a_go = new int[1000]; // 충분한 크기의 배열로 설정
-        int[] b_go = new int[1000]; // 충분한 크기의 배열로 설정
+        int[] a_go = new int[2001]; // 인덱스 0을 사용하지 않고 1부터 시작
+        int[] b_go = new int[2001]; // 인덱스 0을 사용하지 않고 1부터 시작
         
         int cnt_a = 0;
         for(int i = 0; i < n; i++) {
-            int dir = sc.next().equals("L") ? -1 : 1;
+            char dir = sc.next().charAt(0); // 문자열을 char로 변경
             int path = sc.nextInt();
 
             for(int j = 0; j < path; j++) {
-                a_go[cnt_a+1] = a_go[cnt_a] + 1 * dir;  
-                cnt_a += 1;
+                if (dir == 'L') {
+                    a_go[++cnt_a] = a_go[cnt_a - 1] - 1; // 왼쪽으로 이동
+                } else {
+                    a_go[++cnt_a] = a_go[cnt_a - 1] + 1; // 오른쪽으로 이동
+                }
             }              
         }
 
         int cnt_b = 0;
         for(int i = 0; i < m; i++) {
-            int dir = sc.next().equals("L") ? -1 : 1;
+            char dir = sc.next().charAt(0); // 문자열을 char로 변경
             int path = sc.nextInt();
 
             for(int j = 0; j < path; j++) {
-                b_go[cnt_b+1] = b_go[cnt_b] + 1 * dir;  
-                cnt_b += 1;
+                if (dir == 'L') {
+                    b_go[++cnt_b] = b_go[cnt_b - 1] - 1; // 왼쪽으로 이동
+                } else {
+                    b_go[++cnt_b] = b_go[cnt_b - 1] + 1; // 오른쪽으로 이동
+                }
             }              
         }
 
         int ans = 0;
-        for(int i = 1; i < Math.max(cnt_a, cnt_b); i++) {
+        for(int i = 1; i <= Math.max(cnt_a, cnt_b); i++) { // 등호 추가
             if(a_go[i] == b_go[i]) {
                 ans = i;
                 break;
