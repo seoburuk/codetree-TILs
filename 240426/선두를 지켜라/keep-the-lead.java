c971fddad2e73971dbccb475cc510f06dff5e69d
suin <101254699+seoburuk@users.noreply.github.com>
2024-04-26 23:27:14 +0900

    Title: 선두를 지켜라, Time: 113ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240426/선두를 지켜라/keep-the-lead.java
+++ 240426/선두를 지켜라/keep-the-lead.java
@@ -1,48 +1,40 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-        
-        // Arrays to store distances
-        int[] distance_A = new int[n];
-        int[] distance_B = new int[m];
-        
-        // Read A's distances
-        for (int i = 0; i < n; i++) {
+
+        int totalTime = 0;
+        int change = 0;
+        int prevDiff = 0;
+
+        for(int i = 0; i < n; i++){
             int v = sc.nextInt();
             int t = sc.nextInt();
-            distance_A[i] = v * t;
-        }
-        
-        // Read B's distances
-        for (int i = 0; i < m; i++) {
-            int v = sc.nextInt();
-            int t = sc.nextInt();
-            distance_B[i] = v * t;
-        }
-        
-        // Count lead changes
-        int lead_changes = -1;
-        int ahead = 0;  // 0 for tie, 1 for A ahead, -1 for B ahead
-        int minIterations = Math.min(n, m);
-        for (int i = 0; i < minIterations; i++) {
-            if (distance_A[i] > distance_B[i]) {
-                if (ahead != 1) {
-                    ahead = 1;
-                    lead_changes++;
-                }
-            } else if (distance_A[i] < distance_B[i]) {
-                if (ahead != -1) {
-                    ahead = -1;
-                    lead_changes++;
-                }
+            int cnt = v * t;
+            totalTime += t;
+            int currDiff = cnt - prevDiff;
+            if (currDiff * prevDiff < 0) {
+                change++;
             }
+            prevDiff = cnt;
         }
-        
-        // Print the number of lead changes
-        System.out.println(lead_changes);
+
+        prevDiff = 0;
+        for(int i = 0; i < m; i++){
+            int v = sc.nextInt();
+            int t = sc.nextInt();
+            int cnt = v * t;
+            totalTime += t;
+            int currDiff = cnt - prevDiff;
+            if (currDiff * prevDiff < 0) {
+                change++;
+            }
+            prevDiff = cnt;
+        }
+
+        System.out.print(change);
     }
 }
