88963bd354f4fb65701e0a7f5185a3baf82a67f2
suin <101254699+seoburuk@users.noreply.github.com>
2024-04-26 23:22:55 +0900

    Title: 선두를 지켜라, Time: 135ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240426/선두를 지켜라/keep-the-lead.java
+++ 240426/선두를 지켜라/keep-the-lead.java
@@ -1,40 +1,48 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int m = sc.nextInt();
-
-        int totalTime = 0;
-        int change = 0;
-        int prevDiff = 0;
-
-        for(int i = 0; i < n; i++){
+        
+        // Arrays to store distances
+        int[] distance_A = new int[n];
+        int[] distance_B = new int[m];
+        
+        // Read A's distances
+        for (int i = 0; i < n; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
-            int cnt = v * t;
-            totalTime += t;
-            int currDiff = cnt - prevDiff;
-            if (currDiff * prevDiff < 0) {
-                change++;
-            }
-            prevDiff = cnt;
+            distance_A[i] = v * t;
         }
-
-        prevDiff = 0;
-        for(int i = 0; i < m; i++){
+        
+        // Read B's distances
+        for (int i = 0; i < m; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
-            int cnt = v * t;
-            totalTime += t;
-            int currDiff = cnt - prevDiff;
-            if (currDiff * prevDiff < 0) {
-                change++;
-            }
-            prevDiff = cnt;
+            distance_B[i] = v * t;
         }
-
-        System.out.print(change);
+        
+        // Count lead changes
+        int lead_changes = -1;
+        int ahead = 0;  // 0 for tie, 1 for A ahead, -1 for B ahead
+        int minIterations = Math.min(n, m);
+        for (int i = 0; i < minIterations; i++) {
+            if (distance_A[i] > distance_B[i]) {
+                if (ahead != 1) {
+                    ahead = 1;
+                    lead_changes++;
+                }
+            } else if (distance_A[i] < distance_B[i]) {
+                if (ahead != -1) {
+                    ahead = -1;
+                    lead_changes++;
+                }
+            }
+        }
+        
+        // Print the number of lead changes
+        System.out.println(lead_changes);
     }
 }
