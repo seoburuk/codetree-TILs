8869e19c3d37234ea740370d72db4bf9590abca8
suin <101254699+seoburuk@users.noreply.github.com>
2024-04-29 14:13:44 +0900

    Title: 좌우로 움직이는 로봇, Time: -1ms, Memory: -1MB, Status: Compile Error - CodeTree

--- 240429/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
+++ 240429/좌우로 움직이는 로봇/robot-moving-from-side-to-side.java
@@ -1,46 +1,58 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        
-        int n = sc.nextInt(); // 로봇 A의 움직임 횟수
-        int m = sc.nextInt(); // 로봇 B의 움직임 횟수
-        
-        int[] movesA = new int[50000]; // 로봇 A의 움직임 배열
-        int[] movesB = new int[50000]; // 로봇 B의 움직임 배열
-        
-        // 로봇 A의 움직임 입력
-        for (int i = 0; i < n; i++) {
-            int t = sc.nextInt(); // 움직인 시간
-            char d = sc.next().charAt(0); // 방향 ('L' 또는 'R')
-            movesA[i] = t * (d == 'L' ? -1 : 1); // 왼쪽으로 움직이면 음수, 오른쪽으로 움직이면 양수로 저장
-        }
-        
-        // 로봇 B의 움직임 입력
-        for (int i = 0; i < m; i++) {
-            int t = sc.nextInt(); // 움직인 시간
-            char d = sc.next().charAt(0); // 방향 ('L' 또는 'R')
-            movesB[i] = t * (d == 'L' ? -1 : 1); // 왼쪽으로 움직이면 음수, 오른쪽으로 움직이면 양수로 저장
-        }
-        
-        // 로봇 A와 B가 서로 마주치는 경우를 계산
-        int count = 0;
-        int prevPosA = 0;
-        int prevPosB = 0;
-        
-        for (int i = 0; i < Math.max(n, m); i++) {
-            int posA = prevPosA + (i < n ? movesA[i] : 0); // 로봇 A의 현재 위치 계산
-            int posB = prevPosB + (i < m ? movesB[i] : 0); // 로봇 B의 현재 위치 계산
-            
-            if ((prevPosA < prevPosB && posA >= posB) || (prevPosA > prevPosB && posA <= posB)) {
-                count++; // 서로 마주치는 경우 카운트 증가
+        int n = sc.nextInt(); // Robot A
+        int m = sc.nextInt(); // Robot B
+        int[] arrA = new int[n * 1000];
+        int[] arrB = new int[m * 1000];
+
+        int time = 0;
+        int cnt = 0;
+
+        for(int i = 0; i < n; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0); // Corrected
+
+            for(int j = 0; j < t; j++) {
+                if(d == 'L') {
+                    cnt = -1 * t;
+                    arrA[time] = cnt; // Corrected
+                } else {
+                    cnt = 1 * t;
+                    arrA[time] = cnt; // Corrected
+                }
+                time++; // Increment time
             }
-            
-            prevPosA = posA;
-            prevPosB = posB;
         }
-        
-        System.out.println(count);
+
+        for(int i = 0; i < m; i++) {
+            int t = sc.nextInt();
+            char d = sc.next().charAt(0); // Corrected
+
+            for(int j = 0; j < t; j++) {
+                if(d == 'L') {
+                    cnt = -1 * t;
+                    arrB[time] = cnt; // Corrected
+                } else {
+                    cnt = 1 * t;
+                    arrB[time] = cnt; // Corrected
+                }
+                time++; // Increment time
+            }
+        }
+
+        int cnt = 0;
+        int[] diff = new int[time];
+
+        for(int i = 0; i < time; i++) {
+            diff[i] = arrA[i] - arrB[i];
+            if(diff[i-1] != 0 && diff[i] == 0) {
+                cnt++;
+            } 
+        }
+
+        System.out.println(cnt);
     }
 }
