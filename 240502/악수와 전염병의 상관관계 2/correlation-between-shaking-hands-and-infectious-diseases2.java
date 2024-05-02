9be9b526726610073725c35ec25614e82fcd3444
suin <101254699+seoburuk@users.noreply.github.com>
2024-05-02 13:33:48 +0900

    Title: 악수와 전염병의 상관관계 2, Time: 188ms, Memory: 11MB, Status: Passed - CodeTree

---
+++ 240502/악수와 전염병의 상관관계 2/correlation-between-shaking-hands-and-infectious-diseases2.java
@@ -0,0 +1,66 @@
+import java.util.*;
+
+class Developer {
+    public boolean infect;
+    public int num;
+    public int countInfections;
+
+    public Developer(int num, int k) {
+        infect = false;
+        this.num = num;
+        this.countInfections = k;
+    }
+
+    public void infection() {
+        this.infect = true;
+    }
+}
+
+public class Main {
+    public static int n;
+    public static int[] timeX = new int[251];
+    public static int[] timeY = new int[251];
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        int k = sc.nextInt(), p = sc.nextInt(), t = sc.nextInt();
+        Developer[] d = new Developer[n];
+
+        for (int i = 0; i < n; i ++) {
+            d[i] = new Developer(i, k);
+        }
+        
+        d[p - 1].infection();
+
+        while (t -- > 0) {
+            int time = sc.nextInt();
+            int devX = sc.nextInt();
+            int devY = sc.nextInt();
+            timeX[time] = devX;
+            timeY[time] = devY;
+        }
+
+        for (int i = 0; i < timeX.length; i ++) {
+            if(timeX[i] != 0 && timeY[i] != 0) {
+                if(d[timeX[i] - 1].infect && d[timeX[i] - 1].countInfections > 0) {
+                    if(d[timeY[i] - 1].infect) d[timeY[i] - 1].countInfections --;
+                    d[timeY[i] - 1].infection();
+                    d[timeX[i] - 1].countInfections --;
+                    continue;
+                }
+                if(d[timeY[i] - 1].infect && d[timeY[i] - 1].countInfections > 0) {
+                    if(d[timeX[i] - 1].infect) d[timeX[i] - 1].countInfections --;
+                    d[timeX[i] - 1].infection();
+                    d[timeY[i] - 1].countInfections --;
+                    continue;
+                }
+            }
+        }
+
+        for(int i = 0; i < n; i ++) {
+            if(d[i].infect) System.out.print(1);
+            else System.out.print(0);
+        }
+    }
+}
