ec6ffd1fed59ed6a616c1186f3e1985436715775
suin <101254699+seoburuk@users.noreply.github.com>
2024-05-09 10:21:04 +0900

    Title: 선두를 지켜라 3, Time: 138ms, Memory: 10MB, Status: Wrong Answer - CodeTree

--- 240509/선두를 지켜라 3/keep-the-lead-3.java
+++ 240509/선두를 지켜라 3/keep-the-lead-3.java
@@ -9,8 +9,8 @@ public class Main {
         int M = sc.nextInt(); // Number of movements for B
         
         // Read movement information for A
+        int[] arrA = new int[1001]; // Increase the array size to accommodate potential time differences
         int totalTimeA = 0;
-        int[] arrA = new int[1000];
         for(int i = 0; i < N; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
@@ -20,8 +20,8 @@ public class Main {
         }
         
         // Read movement information for B
+        int[] arrB = new int[1001]; // Increase the array size to accommodate potential time differences
         int totalTimeB = 0;
-        int[] arrB = new int[1000];
         for(int i = 0; i < M; i++) {
             int v = sc.nextInt();
             int t = sc.nextInt();
@@ -32,16 +32,16 @@ public class Main {
         
         // Count the number of times their positions change
         int changes = 0;
-        int prevDiff = 0;
-int minTime = Math.min(totalTimeA, totalTimeB); // Determine the minimum valid index
-for(int i = 0; i < minTime; i++) {
-    int diff = arrA[i] - arrB[i];
-    if (diff != prevDiff) {
-        changes++;
-        prevDiff = diff;
-    }
-}
-
+        int currA = 0;
+        int currB = 0;
+        for(int i = 0; i < Math.max(totalTimeA, totalTimeB); i++) {
+            if (i < totalTimeA) currA += arrA[i];
+            if (i < totalTimeB) currB += arrB[i];
+            if (currA != currB) {
+                changes++;
+                currA = currB = 0; // Reset positions to start comparing from the next moment
+            }
+        }
         
         // Output the result
         System.out.println(changes);
