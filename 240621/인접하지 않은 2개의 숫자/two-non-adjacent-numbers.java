a6e8ccca10d5fb4eaa9b247177bc4c337485d3fc
suin <101254699+seoburuk@users.noreply.github.com>
2024-06-21 22:08:02 +0900

    Title: 인접하지 않은 2개의 숫자, Time: 191ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240621/인접하지 않은 2개의 숫자/two-non-adjacent-numbers.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int arr[] = new int[n];
+        for(int i =0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        int max=0;
+        for(int i = 0; i < n-2; i++) {
+            for(int j = i+2; j < n; j ++) {
+                int sum = arr[i]+arr[j];
+                if(sum > max) 
+                    max = sum;
+            }
+        }
+        System.out.print(max);
+    }
+    
+}
