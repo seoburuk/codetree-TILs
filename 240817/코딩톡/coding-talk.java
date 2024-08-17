8dfc0c626be973c1f79d6b8824154c8d7bc06a17
suin <101254699+seoburuk@users.noreply.github.com>
2024-08-17 23:40:36 +0900

    Title: 코딩톡, Time: 116ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240817/코딩톡/coding-talk.java
@@ -0,0 +1,54 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int n = sc.nextInt(); // Number of users
+        int m = sc.nextInt(); // Number of messages
+        int p = sc.nextInt(); // Specific message number to check
+        
+        // Arrays to store message sender and unread count
+        char[] senders = new char[m];
+        int[] unread = new int[m];
+        
+        // Read the input
+        for (int i = 0; i < m; i++) {
+            senders[i] = sc.next().charAt(0); // Sender of the message
+            unread[i] = sc.nextInt(); // Number of people who have not read the message
+        }
+        
+        // Set of all possible users
+        Set<Character> allUsers = new HashSet<>();
+        for (char c = 'A'; c < 'A' + n; c++) {
+            allUsers.add(c);
+        }
+        
+        // Set of users who have definitely read the p-th message
+        Set<Character> readBy = new HashSet<>();
+        
+        // Process each message
+        for (int i = 0; i < m; i++) {
+            if(i >= p-1) {
+                readBy.add(senders[i]);
+            }
+        }
+        
+        // Determine possible users who may not have read the p-th message
+        Set<Character> possibleUnread = new HashSet<>(allUsers);
+        possibleUnread.removeAll(readBy);
+        
+        // Prepare the result
+        List<Character> resultList = new ArrayList<>(possibleUnread);
+        Collections.sort(resultList);
+        
+        // Print the result
+        if (resultList.isEmpty()) {
+            System.out.print("None");
+        } else {
+            for (char c : resultList) {
+                System.out.print(c + " ");
+            }
+        }
+    }
+}
